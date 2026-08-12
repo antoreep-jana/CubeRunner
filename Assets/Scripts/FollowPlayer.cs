018686815b9a9db420f5509a5f5768a8d2cd372c
Antoreep Jana <antoreepjana@gmail.com>
2026-08-12 14:24:03 +0530

    slight camera update

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,9 @@ public class FollowPlayher : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public Transform playerTransform;
-    public float offset = 5f;
+    public float offsetX = 5f;
+    public float offsetY = 0f;
+    public float offsetZ = 0f;
     void Start()
     {
         
@@ -16,12 +18,14 @@ public class FollowPlayher : MonoBehaviour
     {
         Vector3 cameraPosition = transform.position;
 
-        cameraPosition.x = playerTransform.position.x - offset;
+        cameraPosition.x = playerTransform.position.x - offsetX;
 
         // copying y and z positions and adding those as well
 
         // cameraPosition.y = playerTransform.position.y;
-        cameraPosition.z = playerTransform.position.z;
+
+        // cameraPosition.y = transform.position.y - offsetY;
+        cameraPosition.z = playerTransform.position.z - offsetZ;
 
         transform.position = cameraPosition;
     }
